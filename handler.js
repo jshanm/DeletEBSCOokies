49361fb7b79a0324fbf7d948b4feaea3b376038a
Jayaraman Shanmugam <jshanmugam@ebsco.com>
2018-02-06 10:24:22 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,7 +1,6 @@
 chrome.browserAction.onClicked.addListener(DeletCookie) 
 function DeletCookie()  
 {  
-  alert("Started");
   removeCookies(".auth.devqa.ebsco.zone");
   removeCookies(".auth.integration.ebsco.zone");
   removeCookies(".auth.ebsco.zone");
@@ -15,7 +14,6 @@ function DeletCookie()
 function removeCookies(domain) {
   chrome.cookies.getAll({domain: domain}, function(cookies) {
     for(var i=0; i<cookies.length;i++) {
-        console.log("Deleting Cookie: " + cookies[i].name);
         chrome.cookies.remove({url: "http://" + domain + cookies[i].path, name: cookies[i].name});
         chrome.cookies.remove({url: "https://" + domain + cookies[i].path, name: cookies[i].name});
         chrome.cookies.remove({url: "http;://www" + domain + cookies[i].path, name: cookies[i].name});
